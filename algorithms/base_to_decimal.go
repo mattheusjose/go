94ab1de05ea4b1b29f293b0fda5233a079373c87
Matheus Jose <matheus.amaral@hexalab.com.br>
2022-08-14 13:42:44 -0300

    feat: add base to decimal conversor

---
+++ algorithms/base_to_decimal.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+var digits = map[string]string{
+	"A": "10",
+	"B": "11",
+	"C": "12",
+	"D": "13",
+	"E": "14",
+	"F": "15",
+}
+
+func ToDecimal(numericRep string, base int) int {
+
+	exponentialFactor := len(numericRep) - 1
+	var decimalNumber int
+	var digit string
+
+	for _, bit := range numericRep {
+
+		var result int
+		index := string(bit)
+		mapDigit, hasKey := digits[index]
+
+		digit = string(bit)
+
+		if hasKey {
+			digit = mapDigit
+		}
+
+		numericDigit, err := strconv.Atoi(digit)
+		if err != nil {
+			panic("Error converting numeric value")
+		}
+
+		result = int(math.Pow(float64(base), float64(exponentialFactor))) * int(numericDigit)
+		decimalNumber += result
+		exponentialFactor -= 1
+	}
+
+	return decimalNumber
+}
+
+func main() {
+	base := 2
+
+	fmt.Println(fmt.Sprintf("%v to base %v = %v", "1", 2, ToDecimal("1", base)))
+	fmt.Println(fmt.Sprintf("%v to base %v = %v", "10", 2, ToDecimal("10", base)))
+	fmt.Println(fmt.Sprintf("%v to base %v = %v", "21", 3, ToDecimal("21", 3)))
+	fmt.Println(fmt.Sprintf("%v to base %v = %v", "1110", 2, ToDecimal("1110", 2)))
+	fmt.Println(fmt.Sprintf("%v to base %v = %v", "255", base, ToDecimal("255", 16)))
+	fmt.Println(fmt.Sprintf("%v to base %v = %v", "11", 16, ToDecimal("11", 16)))
+}
